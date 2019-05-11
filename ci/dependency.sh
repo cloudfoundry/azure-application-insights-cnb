e8decff6741909fef2c41e388db818c836677f48
Ben Hale <bhale@pivotal.io>
2019-05-11 12:59:07 +0100

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/dependency.sh
+++ ci/dependency.sh
@@ -12,12 +12,11 @@ commit() {
 }
 
 sha256() {
-  shasum -a 256 ../azure-application-insights-java/applicationinsights-agent-*.jar | cut -f 1 -d ' '
+  shasum -a 256 $1 | cut -f 1 -d ' '
 }
 
 uri() {
-  version=$(cat ../azure-application-insights-java/version)
-  echo "https://github.com/Microsoft/ApplicationInsights-Java/releases/download/${version}/applicationinsights-agent-${version}.jar"
+  echo "https://github.com/Microsoft/ApplicationInsights-Java/releases/download/$(cat ../azure-application-insights-java/version)/$(basename $1)"
 }
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
@@ -26,5 +25,5 @@ git config --local user.email ${GIT_USER_EMAIL}
 
 go build -ldflags='-s -w' -o bin/dependency github.com/cloudfoundry/libcfbuildpack/dependency
 
-bin/dependency azure-application-insights-java "[\d]+\.[\d]+\.[\d]+" $(cat ../azure-application-insights-java/version) $(uri) $(sha256)
+bin/dependency azure-application-insights-java "[\d]+\.[\d]+\.[\d]+" $(cat ../azure-application-insights-java/version) $(uri ../azure-application-insights-java/applicationinsights-agent-*.jar) $(sha256 ../azure-application-insights-java/applicationinsights-agent-*.jar)
 commit azure-application-insights-java $(cat ../azure-application-insights-java/version)
