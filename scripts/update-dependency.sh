d82555bf323028e3da899235b005bfc27b7d0296
Ben Hale <bhale@pivotal.io>
2019-09-13 12:53:23 -0700

    New Dependency Updater
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- scripts/update-dependency.sh
+++ scripts/update-dependency.sh
@@ -1,7 +1,7 @@
 uri() {
-  echo "https://github.com/Microsoft/ApplicationInsights-Java/releases/download/$(version)/$(basename ../${DEPENDENCY}/applicationinsights-agent-*.jar)"
+  echo "https://github.com/Microsoft/ApplicationInsights-Java/releases/download/$(version)/$(basename ../dependency/applicationinsights-agent-*.jar)"
 }
 
 sha256() {
-  shasum -a 256 "../${DEPENDENCY}/applicationinsights-agent-*.jar" | cut -f 1 -d ' '
+  shasum -a 256 ../dependency/applicationinsights-agent-*.jar | cut -f 1 -d ' '
 }
